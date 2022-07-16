c5599b511aff29eebcb98e0fabb7aef475e76bc2
20220702 - Brendan Aird <brendan.aird@artificial-solutions.com>
2022-07-16 14:28:21 -0400

    commit all

---
+++ js/logic.js
@@ -0,0 +1,16 @@
+// Grab variables from HTML doc
+
+var choice1 = document.getElementById("c1");
+var choice2 = document.getElementById("c2");
+var choice3 = document.getElementById("c3");
+
+function computerResults() {
+
+    if ((question = 1 and choice = 1)
+        if (q = 2 and choice = 1)
+            if (q = 3 and choice = 1)
+                if (q = 4 and choice = 1)
+                    if (q=5 and c = 1)
+                    else if (q = 5 and choice = 2)
+        else if (q=2 and choice )
+}
